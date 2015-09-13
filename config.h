bd8b922697ba188c9d7c4d9cc3a1a66309e16fc1
Markus Teich <markus.teich@stusta.mhn.de>
2015-09-13 18:17:25 +0200

    enable antialiasing and autohinting for font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "DejaVu Sans Mono:pixelsize=15:antialias=false:autohint=false";
+static char font[] = "DejaVu Sans Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 0;
 static char shell[] = "/bin/mksh";
 static char *utmp = NULL;
